f522e72b22504c48b6a2d3128db89b76e469d0ae
Your Name <andoaccken@gmail.com>
2021-09-04 18:06:55 -0400

    added toggle dataset commands

--- deepdanbooru/commands/create_project.py
+++ deepdanbooru/commands/create_project.py
@@ -1,7 +1,8 @@
 import os
-
+import shutil
 import deepdanbooru as dd
-
+from pathlib import Path
+import sqlite3
 
 def create_project(project_path, project_context=None):
     """
@@ -13,3 +14,32 @@ def create_project(project_path, project_context=None):
         dd.project.DEFAULT_PROJECT_CONTEXT if project_context is None else project_context, project_context_path)
 
     print(f'New project was successfully created. ({project_path})')
+
+def setup_project_from_tag_file(project_path, tag_file, project_context=None):
+  # setup project using db_path defaulted to tags.txt
+  projp = Path(project_path)
+  dd.commands.create_project(projp,
+    {
+      'database_path': str((projp / 'db.sqlite').resolve()),
+      **(
+        {
+          k: v for k, v in (project_context if project_context else dd.project.DEFAULT_PROJECT_CONTEXT).items() if v is not None
+        }
+      ),
+    }
+  )
+
+  project_context = dd.project.load_context_from_project(projp)
+  db_path = project_context['database_path']
+
+  conn = sqlite3.connect(db_path)
+  curr = conn.cursor()
+  dd.commands.make_posts_table(conn, curr)
+
+  conn.commit()
+  conn.close()
+
+  dd.commands.add_images_to_project(projp, tag_file)
+
+  shutil.copyfile(tag_file, projp / 'tags.txt')
+
--- tests/commands/test_toggle_project_images.py
+++ tests/commands/test_toggle_project_images.py
@@ -1,5 +1,6 @@
 from click.testing import CliRunner
 import deepdanbooru as dd
+from tests.utils import generate_fixture_from_tag_file
 from deepdanbooru.commands.add_images_to_project import parse_tags, read_tag_file, copy_image_to_project, add_images_to_project
 from pathlib import Path
 import sqlite3
@@ -46,4 +47,29 @@ def test_toggle_project_images():
       dd.commands.toggle_project_images_randomly('test_project', 1, to_enable=False)
       assert len(dd.data.load_image_records(db_path, 0)) == 2
 
+      # actual tests
+      dd.commands.toggle_project_images_randomly('test_project', 3, to_enable=True)
+      assert len(dd.data.load_image_records(db_path, 0)) == 3
 
+def test_toggle_validation_project_images_randomly():
+  runner = CliRunner()
+
+  with runner.isolated_filesystem():
+    tags = """
+      file1.jpg\ttag1 tag2
+      file2.jpg\ttag1
+      file3.jpg\ttag3
+    """
+    with open('./tags.txt', 'w') as f:
+      f.write(tags)
+
+    generate_fixture_from_tag_file('./tags.txt')
+
+    dd.commands.setup_project_from_tag_file('test_project', './tags.txt')
+    project_context = dd.project.load_context_from_project('test_project')
+
+    dd.commands.toggle_project_validate_images_randomly('test_project', 1, to_validation = True)
+    assert len(dd.data.load_image_records(project_context['database_path'], 0, use_validation=True)) == 1
+
+    dd.commands.toggle_project_validate_images_randomly('test_project', 3, to_validation = False)
+    assert len(dd.data.load_image_records(project_context['database_path'], 0, use_validation=True)) == 0
